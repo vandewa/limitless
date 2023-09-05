a57a9dca1cef261024913c98850ba9f969fc144d
damar wisnu candra prabowo <damar.wisnu1@gmail.com>
2023-09-05 12:46:32 +0700

    asi

--- app/Http/Livewire/Pages/Desa/Sertifikat.php
+++ app/Http/Livewire/Pages/Desa/Sertifikat.php
@@ -1,13 +1,69 @@
 <?php
 
 namespace App\Http\Livewire\Pages\Desa;
+use Livewire\WithPagination;
+use App\Models\DesaSertifikat;
 
 use Livewire\Component;
 
 class Sertifikat extends Component
 {
+    use WithPagination;
+
+    protected $paginationTheme = 'bootstrap';
+
+    public $desa;
+    public $edit = false;
+    public $idnya;
+    public $form = [
+        'nama' => '',
+        'tahun' => '',
+        'lembaga' => '',
+    ];
+
+    public function changeEdit($id) {
+        $this->idnya = $id;
+        $this->edit = !$this->edit;
+        $this->form = DesaSertifikat::find($this->idnya)->only(['nama', 'tahun', 'lembaga']);
+    }
+
+    public function save() {
+        if($this->edit) {
+            $this->edit();
+
+        } else {
+            DesaSertifikat::create($this->form + ['desa_wisata_id' => $this->desa]);
+        }
+        $this->clear();
+        session()->flash('message', 'Data berhasil disimpan.');
+
+    }
+
+    public function delete($id) {
+        DesaSertifikat::destroy($id);
+        session()->flash('hapus', 'Data berhasil dihapus.');
+    }
+
+    public function edit()
+    {
+        DesaSertifikat::find($this->idnya)->update($this->form);
+    }
+
+    public function clear()  {
+        $this->edit = false;
+        $this->idnya = "";
+        $this->form = [
+            'nama' => '',
+            'tahun' => '',
+            'lembaga' => '',
+        ];
+    }
+
+
     public function render()
     {
-        return view('livewire.pages.desa.sertifikat');
+        return view('livewire.pages.desa.sertifikat', [
+            'post' => DesaSertifikat::where('desa_wisata_id', $this->desa)->paginate(10)
+        ]);
     }
 }
--- app/Http/Livewire/Pages/Desa/Sk.php
+++ app/Http/Livewire/Pages/Desa/Sk.php
@@ -3,9 +3,61 @@
 namespace App\Http\Livewire\Pages\Desa;
 
 use Livewire\Component;
+use Livewire\WithPagination;
+use App\Models\DesaSk;
 
 class Sk extends Component
 {
+    use WithPagination;
+
+    protected $paginationTheme = 'bootstrap';
+
+    public $desa;
+    public $edit = false;
+    public $idnya;
+    public $form = [
+        'nama' => '',
+        'tahun' => '',
+        'lembaga' => '',
+    ];
+
+    public function changeEdit($id) {
+        $this->idnya = $id;
+        $this->edit = !$this->edit;
+        $this->form = DesaSk::find($this->idnya)->only(['nama', 'tahun', 'lembaga']);
+    }
+
+    public function save() {
+        if($this->edit) {
+            $this->edit();
+
+        } else {
+            DesaSk::create($this->form + ['desa_wisata_id' => $this->desa]);
+        }
+        $this->clear();
+        session()->flash('message', 'Data berhasil disimpan.');
+
+    }
+
+    public function delete($id) {
+        DesaSk::destroy($id);
+        session()->flash('hapus', 'Data berhasil dihapus.');
+    }
+
+    public function edit()
+    {
+        DesaSk::find($this->idnya)->update($this->form);
+    }
+
+    public function clear()  {
+        $this->edit = false;
+        $this->idnya = "";
+        $this->form = [
+            'nama' => '',
+            'tahun' => '',
+            'lembaga' => '',
+        ];
+    }
     public function render()
     {
         return view('livewire.pages.desa.sk');
